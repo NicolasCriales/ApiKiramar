86bcec2b1877252c27bedfebf54661fdc57a7dba
Santiago Restrepo <auxsistemas1@kiramar.com.co>
2022-09-08 16:31:34 -0500

    paginado

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,41 +1,40 @@
 import { getConnection, sql } from '../database/connection'
 import { tsqlproducts } from '../tsql'
+import { pagination } from '../helpers/pagination'
 
-const getproducts = async (req,res) =>{
+const getproducts = async (req, res) => {
     try {
-        const pool = await getConnection();
         const { IdListaPrecios } = req.body
-        const i = 0
+        const { page, limit } = req.query
+        const pool = await getConnection();
         const result = await pool
-                .request()
-                .input('IdListaPrecios', sql.VarChar,IdListaPrecios)
-                .query(tsqlproducts.getproducts)
+            .request()
+            .input('IdListaPrecios', sql.VarChar, IdListaPrecios)
+            .query(tsqlproducts.getproducts)
         if (result.rowsAffected[0] > 0) {
+            let products = result.recordsets[0]
+            products = await pagination(products, page, limit)
             res.send({
-                products:  result.recordsets
+                products: products
             })
-        } else {
-            res.status(500).json({
-                message: "No se encontro la lista de precios"
-            })  
         }
     } catch (error) {
         console.log('Error: No se pudo consultar los productos ', error)
         res.status(500).json({
             message: 'Problemas al consultar los productos',
-        })  
+        })
     }
 }
 
-const getproducts_individually = async (req,res) =>{
+const getproducts_individually = async (req, res) => {
     try {
         const { IdArticulo, IdListaPrecios } = req.body
         const pool = await getConnection();
         const result = await pool
-                .request()
-                .input('IdArticulo', sql.VarChar,IdArticulo)
-                .input('IdListaPrecios', sql.VarChar,IdListaPrecios)
-                .query(tsqlproducts.getproducts_individually)
+            .request()
+            .input('IdArticulo', sql.VarChar, IdArticulo)
+            .input('IdListaPrecios', sql.VarChar, IdListaPrecios)
+            .query(tsqlproducts.getproducts_individually)
         if (result.rowsAffected[0] > 0) {
             res.send({
                 products_individually: result.recordsets
@@ -43,25 +42,25 @@ const getproducts_individually = async (req,res) =>{
         } else {
             res.status(500).json({
                 message: "No se encontro resultado, verificar los datos"
-            })  
+            })
         }
     } catch (error) {
         console.log('Error: No se pudo consultar los productos individuales ', error)
         res.status(500).json({
             message: 'Problemas al consultar los productos individuales',
-        })  
+        })
     }
 }
 
 
-const getproducts_discount = async (req,res) =>{
+const getproducts_discount = async (req, res) => {
     try {
         const pool = await getConnection();
         const { IdListaPrecios } = req.body
         const result = await pool
-                .request()
-                .input('IdListaPrecios', sql.VarChar,IdListaPrecios)
-                .query(tsqlproducts.getproducts_discount)
+            .request()
+            .input('IdListaPrecios', sql.VarChar, IdListaPrecios)
+            .query(tsqlproducts.getproducts_discount)
         if (result.rowsAffected[0] > 0) {
             res.send({
                 products_discount: result.recordsets
@@ -69,19 +68,18 @@ const getproducts_discount = async (req,res) =>{
         } else {
             res.status(500).json({
                 message: "No se encontro la lista de precio del producto con descuento"
-            })  
+            })
         }
     } catch (error) {
         console.log('Error: No se pudo consultar los productos con descuento ', error)
         res.status(500).json({
             message: 'Problemas al consultar los productos con descuento',
-        })  
+        })
     }
 }
 
 module.exports = {
-	getproducts,
+    getproducts,
     getproducts_individually,
     getproducts_discount,
 }
-      
---
+++ src/helpers/pagination.js
@@ -0,0 +1,29 @@
+
+export const pagination = (model, myPage, myLimit) => {
+    const page = parseInt(myPage);
+    const limit = parseInt(myLimit);
+
+    // calcular el índice inicial y final
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const results = {};
+    if (endIndex < model.length) {
+        results.next = {
+            page: page + 1,
+            limit: limit,
+            articles: model.length
+        };
+    }
+
+    if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit,
+            articles: model.length
+        };
+    }
+
+    results.results = model.slice(startIndex, endIndex)
+
+    return results
+}
