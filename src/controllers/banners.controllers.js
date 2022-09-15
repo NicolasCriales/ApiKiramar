b0679a9f32e85eb01410f8451d946ad3b4dadf72
Santiago Restrepo <auxsistemas1@kiramar.com.co>
2022-09-15 15:49:31 -0500

    lanzamientos

--- src/controllers/banners.controllers.js
+++ src/controllers/banners.controllers.js
@@ -1,5 +1,6 @@
 import { getConnection, sql } from '../database/connection'
 import { tsqlbanners } from '../tsql'
+import { pagination } from '../helpers/pagination'
 
 
 const getbanners = async (req,res) => {
@@ -15,13 +16,13 @@ const getbanners = async (req,res) => {
             })
         } else {
             res.status(500).json({
-                message: "No se encontro resultado de ciudades"
+                message: "No se encontro resultado de BANNERS"
             })  
         }
     } catch (error) {
-        console.log('Error: No se pudo consultar las ciudades ', error)
+        console.log('Error: No se pudo consultar los banners ', error)
         res.status(500).json({
-            message: 'Problemas al consultar las ciudades',
+            message: 'Problemas al consultar los banners',
         })  
     }
 
@@ -30,6 +31,7 @@ const getbanners = async (req,res) => {
 const getproducts_banners = async (req,res) => {
     try {
         const pool = await getConnection();
+        const { page, limit } = req.query
         const { IdListaPrecios, LikeNombreAlterno, LikeCodProveedor } = req.body
         const result = await pool
                 .request()
@@ -38,26 +40,81 @@ const getproducts_banners = async (req,res) => {
                 .input('LikeCodProveedor',sql.VarChar,'%' + LikeCodProveedor + '%')
                 .query(tsqlbanners.products_banners)
         if (result.rowsAffected[0] > 0) {
-            const products = result.recordsets[0]
+            let products = result.recordsets[0]
+            products = await pagination(products, page, limit)
             res.send({
                 products
             })
         } else {
             res.status(500).json({
-                message: "No se encontro resultado de ciudades"
+                message: "No se encontro resultado de los productos del banner"
             })  
         }
     } catch (error) {
-        console.log('Error: No se pudo consultar las ciudades ', error)
+        console.log('Error: No se pudo consultar los productos del banner ', error)
         res.status(500).json({
-            message: 'Problemas al consultar las ciudades',
+            message: 'Problemas al consultar los productos del banner',
         })  
     }
+}
 
+const getbanners_releases = async (req,res) => {
+    try {
+        const pool = await getConnection();
+        const result = await pool
+                .request()
+                .query(tsqlbanners.banners_releases)
+        if (result.rowsAffected[0] > 0) {
+            const Banners = result.recordsets[0]
+            res.send({
+                Banners
+            })
+        } else {
+            res.status(500).json({
+                message: "No se encontro resultado de los lanzamientos"
+            })  
+        }
+    } catch (error) {
+        console.log('Error: No se pudo consultar los lanzamientos ', error)
+        res.status(500).json({
+            message: 'Problemas al consultar los lanzamientos',
+        })  
+    }
+}
+
+const getproducts_releases = async (req,res) => {
+    try {
+        const pool = await getConnection();
+        const { page, limit } = req.query
+        const { IdListaPrecios, CodProveedor } = req.body
+        const result = await pool
+                .request()
+                .input('IdListaPrecios',sql.VarChar,IdListaPrecios)
+                .input('CodProveedor',sql.VarChar,CodProveedor)
+                .query(tsqlbanners.products_new)
+        if (result.rowsAffected[0] > 0) {
+            let Banners = result.recordsets[0]
+            Banners = await pagination(Banners, page, limit)
+            res.send({
+                Banners
+            })
+        } else {
+            res.status(500).json({
+                message: "No se encontro resultado de los productos de lanzamiento"
+            })  
+        }
+    } catch (error) {
+        console.log('Error: No se pudo consultar los productos del lanzamiento ', error)
+        res.status(500).json({
+            message: 'Problemas al consultar los productos del lanzamiento',
+        })  
+    }
 }
 
 module.exports = {
 	getbanners,
     getproducts_banners,
+    getbanners_releases,
+    getproducts_releases,
 }
       
--- src/routes/banners.routes.js
+++ src/routes/banners.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { getbanners,getproducts_banners } from '../controllers/index.js'
+import { getbanners,getproducts_banners,getbanners_releases,getproducts_releases } from '../controllers/index.js'
 
 const router = Router()
 
 router.get('/', getbanners)
 router.get('/products', getproducts_banners)
+router.get('/banners_releases', getbanners_releases)
+router.get('/banners_releases/products', getproducts_releases)
+
+
 
 
 
