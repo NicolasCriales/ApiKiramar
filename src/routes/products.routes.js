39a0a5faeb5182c93250306298e199a8ade8d649
Santiago Restrepo <auxsistemas1@kiramar.com.co>
2022-09-22 14:39:44 -0500

    se esta enviando cambios

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,7 +7,7 @@ const router = Router()
 router.get('/',[validateJWT], getproducts)
 router.get('/article', [validateJWT], getproducts_individually)
 router.get('/discount',[validateJWT],  getproducts_discount)
-router.get('/lastunits',[validateJWT],  getproducts_lastunits)
+router.get('/lastunits',  getproducts_lastunits)
 router.get('/recommends',[validateJWT],  getproducts_recommends)
 
 
