aef29b1c55271c1cd3b8a7888a1a8f9be8f5611c
Santiago Restrepo <auxsistemas1@kiramar.com.co>
2022-09-22 14:06:47 -0500

    cambios nuevos

--- src/routes/city.routes.js
+++ src/routes/city.routes.js
@@ -3,7 +3,7 @@ import { validateJWT } from '../middlewares/validate-jwt'
 import { getcity,getdepartment } from '../controllers/index.js'
 
 const router = Router()
-
+///
 router.get('/',[validateJWT],getcity )
 router.get('/department',[validateJWT],getdepartment )
 
