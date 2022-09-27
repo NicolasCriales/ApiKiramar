0329f7e73c225ed2e9bf4f45f21eea2801fc5312
Santiago Restrepo <auxsistemas1@kiramar.com.co>
2022-09-27 13:36:34 -0500

    se corrigio el error

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,7 +7,7 @@ const router = Router()
 router.get('/',[validateJWT], getproducts)
 router.get('/article', [validateJWT], getproducts_individually)
 router.get('/discount',[validateJWT],  getproducts_discount)
-router.get('/lastunits',  [validateJWT],etproducts_lastunits)
+router.get('/lastunits',  [validateJWT],getproducts_lastunits)
 router.get('/recommends',[validateJWT],  getproducts_recommends)
 router.get('/Best_sellers',[validateJWT],  getBest_sellers)
 
