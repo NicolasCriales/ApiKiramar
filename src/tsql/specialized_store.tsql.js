22c3221331297131e0f29e1c271fd9af4c149f9f
Santiago Restrepo <auxsistemas1@kiramar.com.co>
2022-10-25 11:39:25 -0500

    Orden tiendaoficial

--- src/tsql/specialized_store.tsql.js
+++ src/tsql/specialized_store.tsql.js
@@ -1,7 +1,9 @@
 export const tsqlspecialized_store = {
     specialized_store: `
-                        select RTRIM(CODSUMIN) as CODSUMIN ,RTRIM(NOMBRE) as NOMBRE, 'https://dkasa.com.co/media/Imageneskiramar/tiendasoficiales/' + RTRIM(CODSUMIN) + '.jpg' as IMAGEN
-                        from MtSumini 
-                        where swActivo='1' and swTiendaOficial='1' and swAppKiramar='1' and not swAppDkasa='1' and not CODSUMIN in ('1155','1440')
+    select RTRIM(CODSUMIN) as CODSUMIN ,RTRIM(NOMBRE) as NOMBRE,
+    'https://dkasa.com.co/media/Imageneskiramar/tiendasoficiales/' + RTRIM(CODSUMIN) + '.jpg' as IMAGEN, orden
+    from MtSumini 
+    where swActivo='1' and swTiendaOficial='1' and swAppKiramar='1' and not swAppDkasa='1'
+    order by orden asc
     `
 }
